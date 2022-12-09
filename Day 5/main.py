181a4d177a24f02ac3696423f302481aab434ace
jimmybunter01 <65281539+jimmybunter01@users.noreply.github.com>
2022-12-09 16:29:42 +0000

    Completed Day 5 Part II!

--- Day 5/main.py
+++ Day 5/main.py
@@ -42,7 +42,7 @@ def parse_instructions(raw_instructions):
     
     return parsed_instructions
 
-def sort_the_stacks(stacks, instructions):
+def sort_the_stacks_one_by_one(stacks, instructions):
     stacks_to_sort = stacks
 
     for instruction in instructions:
@@ -58,14 +58,47 @@ def sort_the_stacks(stacks, instructions):
 
     return sorted_stacks
 
+def sorted_stacks_groups_at_once(stacks, instructions):
+    stacks_to_sort = stacks
+    
+    for instruction in instructions:
+        no_of_crates = instruction[0]
+        stack_from = instruction[1]
+        stack_to = instruction[2]
+
+        # Need to maintain the order of the stacks for each move. 
+        # May want to use list slices?
+
+        # e.g. If moving 3 crates from 1 to 3
+
+            # 0   [D]        
+            # 1   [N] [C]    
+            # 2   [Z] [M] [P]
+                #  1   2   3 
+        
+        # Move the slice of crates!
+        slice_of_crate_to_move = stacks_to_sort[stack_from][0:no_of_crates]
+        updated_stack = [crate for crate in slice_of_crate_to_move]
+        updated_stack.extend(stacks_to_sort[stack_to])
+        stacks_to_sort[stack_to] = updated_stack
+
+        # Drop the slice of crates!
+        stack_size = len(stacks_to_sort[stack_from])
+        stacks_to_sort[stack_from] = stacks_to_sort[stack_from][no_of_crates:stack_size]
+
+
+    sorted_stacks = stacks_to_sort
+    return sorted_stacks
+
 def main():
     data = read_in_file_to_list('data/input.txt')
-    raw_stacks = data[0:9]
+    raw_stacks = data[0:8]
     raw_instructions = data[10:len(data)+1]
     parsed_stacks = parse_stacks(raw_stacks)
     parsed_instructions = parse_instructions(raw_instructions)
-    sorted_stacks = sort_the_stacks(parsed_stacks, parsed_instructions)
-    
+    #sorted_stacks = sort_the_stacks_one_by_one(parsed_stacks, parsed_instructions)
+    sorted_stacks = sorted_stacks_groups_at_once(parsed_stacks, parsed_instructions)
+
     for i in range(1,10):
         print(sorted_stacks[i][0].strip('[').strip(']'), end='')
 
